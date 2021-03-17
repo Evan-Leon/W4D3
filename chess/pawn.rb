5b121fd39f46a7ddd31ec3647a71193b162d087c
Evan-Leon <evan.leon.j@gmail.com>
2021-03-17 16:53:10 -0400

    Continued work on pawn class

--- chess/pawn.rb
+++ chess/pawn.rb
@@ -1,7 +1,7 @@
 
 class Pawn < Piece
 
-    def symbols
+    def symbol
         "♟︎".colorize(color)
     end
 
@@ -30,28 +30,39 @@ class Pawn < Piece
         end
     end
 
-
     def forward_steps
+        poss_moves = []
+        row_x, col_y = self.pos[0], self.pos[1]
+        new_move = [row_x+forward_dir, col_y]
+        poss_moves << new_move  
+        poss_moves << [row_x + (2* forward_dir), coly_y] if self.at_start_row?
 
+        poss_moves
     end
 
 
     def side_attacks
         row_x, col_y = self.pos[0], self.pos[1]
         possible_moves = []
+
         if self.color == :black
-            new_pos = [row_x + 1, col_y + 1]
-            if self[new_pos].color == :white
-                possible_moves << new_pos
-                return true
-            end
+            new_pos = [[row_x + forward_dir, col_y + forward_dir], [row_x + forward_dir, col_y + (forward_dir*-1)]]
+            new_pos.each do |coords|
+                if self[coords].color == :white
+                    possible_moves << coords 
+                end
+            end  
+        else self.color == :white
+            new_pos = [[row_x - 1, col_y - 1], [row_x - 1, col_y + 1]]
+            new_pos.each do |coords|
+                if self[coords].color == :black
+                    possible_moves << coords 
+                end
+            end   
+
         end
-
         possible_moves
-        
          #return array of poss moves if can side attack       
-
     end
-
 
 end
